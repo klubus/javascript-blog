023ade0245609701319f3b65767be78fa0b8be58
Krystian Kluba <klubakrystian@gmail.com>
2025-09-07 16:26:20 +0200

    handler event

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
-  console.log("Link was clicked!");
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll(".titles a.active");
@@ -10,7 +10,6 @@ const titleClickHandler = function (event) {
   }
 
   /* add class 'active' to the clicked link */
-  console.log("clickedElement:", clickedElement);
   clickedElement.classList.add("active");
 
   /* remove class 'active' from all articles */
@@ -21,10 +20,13 @@ const titleClickHandler = function (event) {
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute("href");
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add("active");
 };
 
 const links = document.querySelectorAll(".titles a");
