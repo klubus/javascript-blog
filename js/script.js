7d6eea03e7c46d4d02f42035ea40ecaf8f4eaf0c
Krystian Kluba <klubakrystian@gmail.com>
2025-09-07 10:26:39 +0200

    class active to clicked link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const titleClickHandler = function (event) {
+  const clickedElement = this;
   console.log("Link was clicked!");
-  console.log(event);
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll(".titles a.active");
@@ -10,6 +10,8 @@ const titleClickHandler = function (event) {
   }
 
   /* add class 'active' to the clicked link */
+  console.log("clickedElement:", clickedElement);
+  clickedElement.classList.add("active");
 
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts article.active");
